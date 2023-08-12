24ebb2b0d06d970f2d809a927898fd4defd402c5
Jakov Filip Sabolicek <ja.lip132@gmail.com>
2023-08-12 10:00:42 +0200

    chore: init fresh project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { GroupsModule } from './groups/groups.module';
-import { QuestionsModule } from './questions/questions.module';
-import { UserLogsModule } from './user-logs/user-logs.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    QuestionsModule,
-    UserLogsModule,
-    GroupsModule,
-    PrismaModule,
-    UsersModule,
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/user-logs/user-logs.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { UserLogsService } from './user-logs.service';
-
-@Controller('user-logs')
-export class UserLogsController {
-  constructor(private readonly userLogsService: UserLogsService) {}
-}
